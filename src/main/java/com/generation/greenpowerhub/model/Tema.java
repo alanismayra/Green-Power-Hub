b36c2b1219b82ea97b497a399bad2a5991d609dc
AlessandraMariana <ale.souzza12@gmail.com>
2023-05-19 09:25:32 -0300

    Adiciona Tema

---
+++ src/main/java/com/generation/greenpowerhub/model/Tema.java
@@ -0,0 +1,64 @@
+package com.generation.greenpowerhub.model;
+
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+
+@Entity
+@Table (name= "tb_temas")
+public class Tema {
+	@Id 
+	@GeneratedValue (strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotNull(message = "Este atributo é de preenchimento obrigatório")
+	private String nome;
+
+	@NotNull(message = "Este atributo é de preenchimento obrigatório")
+	private String descricao;
+	
+	public List<Postagem> getPostagem() {
+		return postagem;
+	}
+
+	public void setPostagem(List<Postagem> postagem) {
+		this.postagem = postagem;
+	}
+
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "tema", cascade = CascadeType.REMOVE)
+	@JsonIgnoreProperties("tema")
+	private List <Postagem> postagem;
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+}
