0806e45181733a2fe1fe3376837b3ac8b3fc36fc
AlessandraMariana <ale.souzza12@gmail.com>
2023-05-19 09:56:22 -0300

    Atualiza PostagemController

--- src/main/java/com/generation/greenpowerhub/controller/PostagemController.java
+++ src/main/java/com/generation/greenpowerhub/controller/PostagemController.java
@@ -5,7 +5,6 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -21,6 +20,7 @@ import org.springframework.web.server.ResponseStatusException;
 
 import com.generation.greenpowerhub.model.Postagem;
 import com.generation.greenpowerhub.repository.PostagemRepository;
+import com.generation.greenpowerhub.repository.TemaRepository;
 
 import jakarta.validation.Valid;
 
@@ -32,6 +32,9 @@ public class PostagemController {
 	
 	@Autowired
 	private PostagemRepository postagemRepository;
+	
+	@Autowired
+	private TemaRepository temaRepository;
 	
 	@GetMapping
 	public ResponseEntity <List<Postagem>> getAll(){
@@ -52,16 +55,23 @@ public class PostagemController {
 	
 	@PostMapping
 	public ResponseEntity<Postagem>post(@Valid @RequestBody Postagem postagem){
+		if(temaRepository.existsById(postagem.getTema().getId()))
 		return ResponseEntity.status(HttpStatus.CREATED)
 				.body(postagemRepository.save(postagem));
+		
+		throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Tema não existe", null);	
 	}
 	
 	@PutMapping
 	public ResponseEntity<Postagem> put(@Valid @RequestBody Postagem postagem){
-		return postagemRepository.findById(postagem.getId())
-				.map(resposta -> ResponseEntity.status(HttpStatus.OK)
-				.body(postagemRepository.save(postagem)))
-				.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+		if(postagemRepository.existsById(postagem.getId())) {
+			
+			if (temaRepository.existsById(postagem.getTema().getId()))
+				return ResponseEntity.status(HttpStatus.OK)
+						.body(postagemRepository.save(postagem));
+				throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Tema não existe", null);	
+				}
+				return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
 	}
 	
 	@ResponseStatus(HttpStatus.NO_CONTENT)
